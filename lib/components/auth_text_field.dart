bfaf7274c1f6ccbf306fd51b97191dc2e4693058
yjjo8136 <yjun8136@gmail.com>
2023-02-14 00:52:58 +0900

    feature: 로그인 페이지 구조 완성

---
+++ lib/components/auth_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AuthTextField extends StatelessWidget {
+  final Icon prefixIcon;
+  final String hintText;
+  final FormFieldSetter onSaved;
+  final FormFieldValidator validator;
+  final bool isPassword;
+
+  const AuthTextField({
+    required this.prefixIcon,
+    required this.hintText,
+    required this.onSaved,
+    required this.validator,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: TextFormField(
+        obscureText: isPassword,
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          hintText: hintText,
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+          ),
+        ),
+        onSaved: onSaved,
+        validator: validator,
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,60 +1,130 @@
-import 'package:daily_carbon/models/letter.dart';
+import 'package:daily_carbon/components/auth_text_field.dart';
 import 'package:daily_carbon/pages/signup_page.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final formKey = GlobalKey<FormState>();
+
+  String id = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _appLogoImage(context),
+                SizedBox(height: 30),
+                _loginForm(context),
+                SizedBox(height: 20),
+                _signupNavigateButton(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _appLogoImage(BuildContext context) {
+    return Image(
+      image: AssetImage('assets/app_logo.png'),
+      fit: BoxFit.contain,
+      height: 200,
+      width: MediaQuery.of(context).size.width * 0.8,
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
         children: [
-          Text("Login"),
-          InkWell(
-            child: Container(
-              width: 60,
-              height: 30,
-              color: Colors.lightGreen,
-              child: Center(child: Text("Login")),
+          Padding(
+            padding: const EdgeInsets.only(left: 32),
+            child: Row(
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
             ),
-            onTap: () async {
-              Response response;
-              var dio = Dio();
-              response = await dio.post('http://10.0.2.2:3000/login', data: {
-                'id': 'qwer',
-                'password': '123',
-              });
-              print(response);
+          ),
+          SizedBox(height: 30),
+          AuthTextField(
+            prefixIcon: Icon(Icons.person_outline),
+            hintText: 'ID',
+            onSaved: (val) => {id = val},
+            validator: (val) {
+              if (val.length < 1) {
+                return '아이디를 입력해주세요';
+              }
             },
           ),
-          InkWell(
-            child: Text(
-              "Sign Up",
-              style: TextStyle(color: Colors.blue),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignupPage()),
-              );
+          SizedBox(height: 15),
+          AuthTextField(
+            prefixIcon: Icon(Icons.lock_outline_rounded),
+            hintText: 'Password',
+            onSaved: (val) => {password = val},
+            isPassword: true,
+            validator: (val) {
+              if (val.length < 1) {
+                return '패스워드를 입력해주세요';
+              }
             },
           ),
-          InkWell(
-            child: Container(
-              color: Colors.blue,
-              child: Text("Get 요청"),
-            ),
-            onTap: () async {
-              Response response;
-              var dio = Dio();
-              response = await dio.get('http://10.0.2.2:3000/meetings/7');
-              print(Letter.fromJson(response.data[0]).author);
-            },
-          )
+          SizedBox(height: 30),
+          _loginSubmitButton(context),
         ],
       ),
+    );
+  }
+
+  Widget _loginSubmitButton(BuildContext context) {
+    return InkWell(
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.7,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.lightGreen,
+        ),
+        child: Center(child: Text("Login")),
+      ),
+      onTap: () {
+        if (formKey.currentState!.validate()) {
+          formKey.currentState!.save();
+          print("ID: ${id}");
+          print("password: ${password}");
+        }
+      },
+    );
+  }
+
+  Widget _signupNavigateButton(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("New to LoveLetter?  "),
+        InkWell(
+          child: Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SignupPage()),
+            );
+          },
+        ),
+      ],
     );
   }
 }
