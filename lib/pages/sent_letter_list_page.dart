646de12d9939dfd95b904342a9702bc68cc86e62
yjjo8136 <yjun8136@gmail.com>
2023-02-15 08:57:10 +0900

    feature: User Info Page 구조 완성

---
+++ lib/pages/sent_letter_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SentLetterListPage extends StatelessWidget {
+  const SentLetterListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/pages/user_info_page.dart
+++ lib/pages/user_info_page.dart
@@ -1,10 +1,83 @@
+import 'package:daily_carbon/components/receiver_profile_tile.dart';
+import 'package:daily_carbon/pages/received_letter_list_page.dart';
 import 'package:flutter/material.dart';
+
+import 'sent_letter_list_page.dart';
 
 class UserInfoPage extends StatelessWidget {
   const UserInfoPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ReceiverProfileTile(),
+            _buildSentLetterButton(context),
+            SizedBox(height: 10),
+            _buildReceivedLetterButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSentLetterButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: InkWell(
+        child: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: 10),
+              Icon(Icons.email),
+              SizedBox(width: 20),
+              Text("보낸 편지함  00통"),
+            ],
+          ),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SentLetterListPage()),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildReceivedLetterButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: InkWell(
+        child: Container(
+          height: 60,
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: 10),
+              Icon(Icons.email),
+              SizedBox(width: 20),
+              Text("받은 편지함  00통"),
+            ],
+          ),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ReceivedLetterListPage()),
+          );
+        },
+      ),
+    );
   }
 }
